2d0a026c949df946791bc5d08f723d98945b2dde
Rémi Defoor <remi.defoor@student.howest.be>
2020-12-14 12:17:51 +0100

    get currently active and next to display page

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,12 +2,14 @@
 
 /* Declare any global variables below this line, but before the first function call */
 
+document.addEventListener('DOMContentLoaded', init);
+
 function init() {
 
     // Fillings
 
     // Bindings
-
+    document.querySelectorAll('.navigation-links a').forEach(a => a.addEventListener('click', handleNavigation));
     // Delegates
 
     // Other initialisations
@@ -16,6 +18,12 @@ function init() {
 
 
 function handleNavigation(e) {
+    e.preventDefault();
+    const currentPage = document.querySelector
+    ('#left-aligned-content section:not(.hidden), #left-aligned-content > article:not(.hidden)');
+    const nextPage = e.target.getAttribute("href");
+    console.log(currentPage);
+    console.log(nextPage);
 
 }
 
