2f54c985b0b5c82a46bdc3ed9eaaf00b3170858a
Rémi Defoor <remi.defoor@student.howest.be>
2020-12-15 08:44:06 +0100

    - show next page - show previous page

--- assets/js/personal-data.js
+++ assets/js/personal-data.js
@@ -3,8 +3,20 @@
 document.addEventListener("DOMContentLoaded", init);
 
 function init() {
-    //Delegates
+    // fillings
+    restoreUserData();
+    // delegates
     document.forms["personal-data"].addEventListener("submit", processPersonalData);
+}
+
+function restoreUserData() {
+    const user = JSON.parse(localStorage.getItem("person"));
+    if (user) {
+        const form = document.forms["personal-data"];
+        form["name"].value = user["name"];
+        form["email"].value = user["email"];
+        form["available-ECTS"].value = user["ECTS"];
+    }
 }
 
 function processPersonalData(e) {
@@ -13,6 +25,9 @@ function processPersonalData(e) {
     person["name"] = e.currentTarget["name"].value;
     person["email"] = e.currentTarget["email"].value;
     person["ECTS"] = parseInt(e.currentTarget["available-ECTS"].value);
+    if (validateData(person)) {
+        savePersonalData(person);
+    }
 }
 
 function validateData(person){
@@ -29,7 +44,9 @@ function validateData(person){
 }
 
 function savePersonalData(person){
-
+    localStorage.setItem("person", JSON.stringify(person));
+    document.querySelector("#personal-data").classList.add("hidden");
+    document.querySelector("#completed-courses").classList.remove("hidden");
 }
 
 //export {processPersonalData};
