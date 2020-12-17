e2ea1c28c54d1c093aa004506a80532db7b98cad
Rémi Defoor <remi.defoor@student.howest.be>
2020-12-17 18:48:49 +0100

    programmed fill table footer

--- assets/js/summary.js
+++ assets/js/summary.js
@@ -4,7 +4,10 @@ function fillSummary(e) {
     let completedECTS = computeTotalECTS(completedModules).toString();
     document.querySelector("#overview #completed-ECTS-counter p").innerHTML = completedECTS;
     let withdrawnECTS = getWithdrawnECTS();
-    document.querySelector("#overview #desired-ECTS-counter p").innerHTML = getWithdrawnECTS();
+    document.querySelector("#overview #desired-ECTS-counter p").innerHTML = withdrawnECTS;
+    const table = document.querySelector("#overview table");
+    fillTbody(table);
+    filltfoot(table, completedECTS, withdrawnECTS);
 }
 
 
@@ -12,6 +15,9 @@ function fillTbody(parent) {
 
 }
 
-function filltfoot(parent) {
-
+function filltfoot(parent, completedECTS, withdrawnECTS) {
+    completedECTS = `<th class="left-part-table">${completedECTS}</th>`;
+    parent.querySelector("tfoot th.left-part-table").insertAdjacentHTML("afterend", completedECTS);
+    withdrawnECTS = `<th class="right-part-table">${withdrawnECTS}</th>`;
+    parent.querySelector("tfoot th.right-part-table").insertAdjacentHTML("afterend", withdrawnECTS);
 }
