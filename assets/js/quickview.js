4163175ebefeae7e4b5c7f693e72a53ac46bf187
Rémi Defoor <remi.defoor@student.howest.be>
2020-12-17 11:51:18 +0100

    programmed progress bar

--- assets/js/quickview.js
+++ assets/js/quickview.js
@@ -1,15 +1,36 @@
 "use strict";
 
 function fillQuickview() {
-    let allocatedECTS = computeAllocatedECTS().toString();
+    let allocatedECTS = computeAllocatedECTS();
     document.querySelector("#allocated-ECTS").innerHTML = allocatedECTS;
     let withdrawnECTS = getWithdrawnECTS();
-    let unallocatedECTS = withdrawnECTS - allocatedECTS.toString();
+    let unallocatedECTS = withdrawnECTS - allocatedECTS;
     document.querySelector("#unallocated-ECTS").innerHTML = `${unallocatedECTS} ECTS left`;
+    progressBar(withdrawnECTS, allocatedECTS);
 }
 
 function progressBar(desiredECTS, totalECTS){
+    let percentageAllocatedECTS = Math.round((totalECTS / desiredECTS) * 100);
+    document.querySelector("#percentage-allocated-ECTS").innerHTML = `${percentageAllocatedECTS}% desired ects allocated`;
+    const progressBar = document.querySelector("#progress-bar");
+    progressBar.className = determineProgressBarColor(percentageAllocatedECTS);
+    progressBar.style.width = `${percentageAllocatedECTS}%`;
+}
 
+function determineProgressBarColor(percentageAllocatedECTS) {
+    let color;
+    switch (true) {
+        case (percentageAllocatedECTS < 25):
+            return  color = "pink";
+        case (percentageAllocatedECTS < 50):
+            return color = "orange";
+        case (percentageAllocatedECTS < 75):
+            return  color = "yellow";
+        case (percentageAllocatedECTS < 100):
+            return color = "green";
+        default:
+            return color = "white";
+    }
 }
 
 function ectsPerSemester(){
